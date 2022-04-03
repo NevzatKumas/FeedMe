2103e02615b3f476bce8465f16dc353cb2144e6d
Abed-2000 <64762826+Abed-2000@users.noreply.github.com>
2022-04-02 20:52:28 -0400

    added dbName

--- src/dbConnect.php
+++ src/dbConnect.php
@@ -2,7 +2,8 @@
 $servername = "sql1.njit.edu";
 $username = "am2449";
 $password = "RedRaceCar123!";
-$con = mysqli_connect($servername,$username,$password);
+$dbName = "am2449";
+$con = mysqli_connect($servername,$username,$password,$dbName);
 
 if ($con->connect_error) {
   die("Connection failed: " . $conn->connect_error);
